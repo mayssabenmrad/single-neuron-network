f5ceca56435914b64f209089db3a677d8e730aa8
Amanullah Menjli <menjliamanullah@skiff.com>
2024-04-10 21:04:34 +0100

    fix: wrong params type in the loss function header
    Switch the parameters type of the loss function from floats to pointers in
    the header file to match the source file.

--- loss_function.h
+++ loss_function.h
@@ -1,3 +1,3 @@
 // Calculate the loss value of a neuron on a given dataset using the Cross
 // Entropy Loss function.
-float cross_entropy_loss(float y, float y_hat, int N);
+float cross_entropy_loss(float *y, float *y_hat, int N);
