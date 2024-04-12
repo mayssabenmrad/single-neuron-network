6ee187a461486a2f39e98b849ec778bb4ac902ff
Amanullah Menjli <menjliamanullah@skiff.com>
2024-04-12 23:16:29 +0100

    fix(train): update the header file
    Update the declaration for the train function to match the source file.

--- train.h
+++ train.h
@@ -1,5 +1,5 @@
 #include "neuron.h"
 
 // Train the neuron using backpropagation
-int train(Neuron *neuron, float **inputs, float *outputs, int N, int epochs,
-	  float learning_rate);
+int train(Neuron *neuron, float **inputs, float *outputs, int samples_num,
+	  float learning_rate, float epsilon);
